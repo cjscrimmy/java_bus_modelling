03a42553769cfd15f002881e5f2543f8f2a4fa72
cjscrimmy <craigscrimgeour@yahoo.com>
2020-06-16 14:21:45 +0100

    create tests for destination and capacity and code to make tests pass

--- src/test/java/BusTest.java
+++ src/test/java/BusTest.java
@@ -1,2 +1,25 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BusTest {
+
+    private Bus bus;
+
+    @Before
+    public void before(){
+        bus = new Bus("Ocean Terminal", 50);
+    }
+
+    @Test
+    public void hasDestination(){
+        assertEquals("Ocean Terminal", bus.getDestination());
+    }
+
+    @Test
+    public void hasCapacity(){
+        assertEquals(50, bus.getCapacity());
+    }
+
 }
